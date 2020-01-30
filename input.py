5ee5ba65ce13a3e45d9b0ff4de5defeace5c38f2
JackNarowski <jacknarowski@gmail.com>
2020-01-29 19:40:20 -0600

    Added TrueFalseQuestionType

--- input.py
+++ input.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-def Sample():
+def Sample(): #FOR REFERENCE OF HOW TO USE ET
     #Set up tree
     data = ET.Element('data')
     items = ET.SubElement(data, 'items')
@@ -22,37 +22,64 @@ def initXML():
     data = ET.Element('data')
     return data
 
-def MultipleChoiceQuestion(data, question1in, option1in, option2in, option3in, option4in, option5in, answer1in):
+def MultipleChoiceQuestion(data, questionin, option1in, option2in, option3in, option4in, option5in, answerin):
     #Set up tree
     MultipleChoiceQuestionType = ET.SubElement(data, 'MultipleChoiceQuestionType')
-    question1 = ET.SubElement(MultipleChoiceQuestionType,'question1')
+    question = ET.SubElement(MultipleChoiceQuestionType,'question')
     options = ET.SubElement(MultipleChoiceQuestionType, 'options')
     option1 = ET.SubElement(options, 'option')
     option2 = ET.SubElement(options, 'option')
     option3 = ET.SubElement(options, 'option')
     option4 = ET.SubElement(options, 'option')
     option5 = ET.SubElement(options, 'option')
-    answer1 = ET.SubElement(MultipleChoiceQuestionType,'answer1')
+    answer = ET.SubElement(MultipleChoiceQuestionType,'answer')
 
     #Set names
-    question1.set('name','question1')
+    question.set('name','question')
     option1.set('name','option1')
     option2.set('name', 'option2')
     option3.set('name', 'option3')
     option4.set('name', 'option4')
     option5.set('name','option5')
-    answer1.set('name','answer1')
+    answer.set('name','answer')
 
     #Fill values
-    question1.text = question1in
+    question.text = questionin
     option1.text = option1in
     option2.text = option2in
     option3.text = option3in
     option4.text = option4in
     option5.text = option5in
-    answer1.text = answer1in
+    answer.text = answerin
 
     return(data)
+
+def TrueFalseQuestion(data, questionin, answerin):
+    #Setup Tree
+    TrueFalseQuestionType = ET.SubElement(data, 'TrueFalseQuestionType')
+    question = ET.SubElement(TrueFalseQuestionType, 'question')
+    options = ET.SubElement(TrueFalseQuestionType, 'options')
+    true = ET.SubElement(options, 'option')
+    false = ET.SubElement(options, 'option')
+    answer = ET.SubElement(TrueFalseQuestionType, 'answer')
+
+    #Set Names
+    question.set('name', 'question')
+    true.set('name', 'true')
+    false.set('name', 'false')
+    answer.set('name', 'answer')
+
+    #Fill Values
+    question.text = questionin
+    answer.text = answerin
+    true.text = "True"
+    false.text = "False"
+
+    return(data)
+
+
+
+
 
 def ExportToXML (data):
     mydata = ET.tostring(data)
