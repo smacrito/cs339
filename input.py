a27324ea024b5fd999984e193cb4adb2cd5a181b
JackNarowski <jacknarowski@gmail.com>
2020-01-29 19:33:55 -0600

    restructred and added MultipleChoiceQuestion type

--- input.py
+++ input.py
@@ -1,7 +1,6 @@
 import xml.etree.ElementTree as ET
 
-
-def func():
+def Sample():
     #Set up tree
     data = ET.Element('data')
     items = ET.SubElement(data, 'items')
@@ -19,4 +18,43 @@ def func():
     myfile = open("temp.xml","w")
     myfile.write(mydata)
 
-func()
+def initXML():
+    data = ET.Element('data')
+    return data
+
+def MultipleChoiceQuestion(data, question1in, option1in, option2in, option3in, option4in, option5in, answer1in):
+    #Set up tree
+    MultipleChoiceQuestionType = ET.SubElement(data, 'MultipleChoiceQuestionType')
+    question1 = ET.SubElement(MultipleChoiceQuestionType,'question1')
+    options = ET.SubElement(MultipleChoiceQuestionType, 'options')
+    option1 = ET.SubElement(options, 'option')
+    option2 = ET.SubElement(options, 'option')
+    option3 = ET.SubElement(options, 'option')
+    option4 = ET.SubElement(options, 'option')
+    option5 = ET.SubElement(options, 'option')
+    answer1 = ET.SubElement(MultipleChoiceQuestionType,'answer1')
+
+    #Set names
+    question1.set('name','question1')
+    option1.set('name','option1')
+    option2.set('name', 'option2')
+    option3.set('name', 'option3')
+    option4.set('name', 'option4')
+    option5.set('name','option5')
+    answer1.set('name','answer1')
+
+    #Fill values
+    question1.text = question1in
+    option1.text = option1in
+    option2.text = option2in
+    option3.text = option3in
+    option4.text = option4in
+    option5.text = option5in
+    answer1.text = answer1in
+
+    return(data)
+
+def ExportToXML (data):
+    mydata = ET.tostring(data)
+    myfile = open("temp.xml", "w")
+    myfile.write(mydata)
