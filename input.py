2d4b42ea218f8882ecc3fdbc304d1220ac3c39f8
JackNarowski <jacknarowski@gmail.com>
2020-01-29 19:41:43 -0600

    Removed Sample method

--- input.py
+++ input.py
@@ -1,23 +1,5 @@
 import xml.etree.ElementTree as ET
 
-def Sample(): #FOR REFERENCE OF HOW TO USE ET
-    #Set up tree
-    data = ET.Element('data')
-    items = ET.SubElement(data, 'items')
-    item1 = ET.SubElement(items,'item')
-    item2 = ET.SubElement(items,'item')
-
-    #Add values
-    item1.set('name', 'item1')
-    item2.set('name','item2')
-    item1.text = "question"
-    item2.text = 'question'
-
-    #Export
-    mydata = ET.tostring(data)
-    myfile = open("temp.xml","w")
-    myfile.write(mydata)
-
 def initXML():
     data = ET.Element('data')
     return data
@@ -77,6 +59,7 @@ def TrueFalseQuestion(data, questionin, answerin):
 
     return(data)
 
+#def ShortResponseQuestion(data, questionin):
 
 
 
