a65b9ead5973f6265cfbd3fd8d538277ba355a68
fiyyaa <smacrito@luc.edu>
2020-01-31 17:26:54 -0600

    completed addQuestionForm to accept an answer and question from user input.

--- flask/forms.py
+++ flask/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 #create a separate file for forms for organization
 #create a class for each form that we can call later
 class addQuestionForm(FlaskForm):                   #calling premade function to check for input
-    userQuestion = StringField('Question', validators=[DataRequired()])
+    userQuestion = StringField('Question', validators=[DataRequired(), Length(min=1,max=500)])
+    userAnswer = StringField('Answer', validators=[DataRequired(), Length(min=1,max=500)])
+    confirm = SubmitField('Add Question')
