45088803fec2696c2f6c57011408c93a777d59f1
fiyyaa <smacrito@luc.edu>
2020-01-31 17:32:25 -0600

    added forms and completed it. TODO: finish addQuestion route to accept questions from user input. Also change home template or make a new one for addQuestion

--- flask/FlaskClient.py
+++ flask/FlaskClient.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+#passing class in from forms.py
+from forms import addQuestionForm
 #import xmltodict
 
 app = Flask(__name__)
+
 
 #with open('C:/Users/Sammy/Desktop/School/COMP339/cs339/flask/demo2.xml') as fd:
 #   doc = xmltodict.parse(fd.read())
@@ -17,7 +20,7 @@ questions = [
 def hello():
     return render_template('home.html', questions=questions)
 
-
+@app.route('/addQuestion')
 
 
 
