88702d56d923a1a2c629753171657ae5df96d7cf
fiyyaa <smacrito@luc.edu>
2020-02-02 00:29:35 -0600

    added CSRF protection to allow forms. Added forms, addQuestion template.

--- flask/FlaskClient.py
+++ flask/FlaskClient.py
@@ -4,6 +4,7 @@ from forms import addQuestionForm
 #import xmltodict
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'test'
 
 
 #with open('C:/Users/Sammy/Desktop/School/COMP339/cs339/flask/demo2.xml') as fd:
@@ -17,11 +18,13 @@ questions = [
 ]
 
 @app.route('/')
-def hello():
+def home():
     return render_template('home.html', questions=questions)
 
 @app.route('/addQuestion')
-
+def addQuestion():
+    form = addQuestionForm()
+    return render_template('addQuestion.html', title='Add Question', form=form)
 
 
 #allows running flask from python directly, doesnt require env vars
