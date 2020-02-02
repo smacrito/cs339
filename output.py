ebed98490d17804ab53eef394a459de34fe3e16c
JackNarowski <jacknarowski@gmail.com>
2020-02-02 16:42:23 -0600

    added GetQuestionCount... might modify how it works later

--- output.py
+++ output.py
@@ -3,3 +3,10 @@ import xml.etree.ElementTree as ET
 def ReadXML(path):
     tree = ET.parse(path)
     return tree
+
+def GetQuestionCount(tree):
+    count = 0
+    root = tree.getroot()
+    for child in root:
+        count+=1
+    return count
