f0cb000fcc9dffb07857c735c1203764fa5dfec8
JackNarowski <jacknarowski@gmail.com>
2020-02-02 16:50:40 -0600

    added GetQuestion function and FindType subfunction

--- output.py
+++ output.py
@@ -10,3 +10,11 @@ def GetQuestionCount(tree):
     for child in root:
         count+=1
     return count
+
+def GetQuestion(number, tree):
+    question = tree[number] #gets question
+    FindType(question[0]) #will then use this function to find type
+    #will use that type to decide what subfunction to use
+
+def FindType():
+    return 0
