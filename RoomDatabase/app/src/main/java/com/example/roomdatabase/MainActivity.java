9cf487bd84aa6bae95780817388e54f83a643613
fiyyaa <smacrito@luc.edu>
2020-03-16 16:59:10 -0500

    Added buttons for getting to pages to eventually add to database"

--- RoomDatabase/app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ RoomDatabase/app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.roomdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-
+//TODO: add functions to buttons and adjust layout to make them lower
 public class MainActivity extends AppCompatActivity {
-
+    //create frag manager object
+    public static FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // adding instance of fragmanager
+        fragmentManager = getSupportFragmentManager();
+
+        if(findViewById(R.id.fragment_container)!=null){
+            if(savedInstanceState!=null){
+                return;
+            }
+            //adds fragment to main activity
+            fragmentManager.beginTransaction().add(R.id.fragment_container,new HomeFragment()).commit();
+        }
     }
 }
