fb08a7ff3c6a4ed208b0c89c04f74108f469c1f7
JackNarowski <jacknarowski@gmail.com>
2020-04-14 19:55:28 -0500

    Added hello

--- RoomDatabase/app/src/main/java/com/example/roomdatabase/UDP_Client.java
+++ RoomDatabase/app/src/main/java/com/example/roomdatabase/UDP_Client.java
@@ -11,7 +11,34 @@ import java.util.List;
 
 
 public class UDP_Client {
+    public static String client_helloworld() throws Exception {
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        //BufferedReader inFromUser=new BufferedReader(new InputStreamReader(System.in));
+
+        DatagramSocket clientSocket=new DatagramSocket();
+        InetAddress IPAddress=InetAddress.getByName("localhost");
+
+        byte[]sendData=new byte[1024];
+        byte[]receiveData=new byte[1024];
+
+        //String sentence= "Hello worlds";
+        List<Question> question = MainActivity.QuestionDB.dao().getQuestion();
+        String questionToJSON = new Gson().toJson(question);
+
+
+        sendData=questionToJSON.getBytes();
+
+        DatagramPacket sendPacket=new DatagramPacket(sendData,sendData.length,IPAddress,9876);
+        clientSocket.send(sendPacket);
+        DatagramPacket receivePacket=new DatagramPacket(receiveData,receiveData.length);
+        clientSocket.receive(receivePacket);
+        String modifiedSentence=new String(receivePacket.getData());
+        clientSocket.close();
+        return modifiedSentence;
+    }
 
     public static int clientSend() throws Exception{
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -37,15 +64,20 @@ public class UDP_Client {
     }
 
     public static String clientGet() throws Exception{
+        System.out.println("Pos1");
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-
+        System.out.println("Pos2");
         byte[]receiveData=new byte[1024];
         DatagramSocket clientSocket = new DatagramSocket();
         DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
+        System.out.println("Pos3");
         clientSocket.receive(receivePacket);
+        System.out.println("Pos4");
         String modifiedSentence = new String(receivePacket.getData());
+        System.out.println("Pos5");
         clientSocket.close();
+        System.out.println("Pos6");
         return modifiedSentence;
     }
 }
