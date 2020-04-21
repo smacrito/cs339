641911f25eab75816fcb1f095d91fe285fb15c39
JackNarowski <jacknarowski@gmail.com>
2020-04-21 15:18:36 -0500

    Added additional features to Startup.java

--- RoomDatabase/app/src/main/java/com/example/roomdatabase/Startup.java
+++ RoomDatabase/app/src/main/java/com/example/roomdatabase/Startup.java
@@ -4,6 +4,9 @@ public class Startup {
     int address; //Will match port for UDP_Client
     int port;
 
+    UDP_Server udp = new UDP_Server();
+    UDP_Client udpClient = new UDP_Client();
+
     public int getAddress() {
         return address;
     }
@@ -15,6 +18,7 @@ public class Startup {
     public void host(){
         //generate address
         //start up server
+        udp.execute();
         //return address
     }
 
@@ -22,6 +26,17 @@ public class Startup {
         this.address=address;
         this.port = port;
         //Start running UDP_Client
+        //Schedule this
+        udpClient.execute(); //udp getter
+
+        //udp sender to go at the end of each question submission
+        try{
+            System.out.println("send_data_button clicked");
+            udpClient.clientSend();
+        }
+        catch(Exception E){
+            System.out.println(E);
+        }
     }
 
 }
